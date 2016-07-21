d237ed13b7a368d8cb1053346b4b4566018f4fcd
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2016-07-21 14:50:38 +0200

    Attach additional informations to nodes

--- js/node_logic.js
+++ js/node_logic.js
@@ -4,21 +4,37 @@
 
 var tree = {}
 var resolveCache = {};
+var meta = {};
 
 // ------------------------------------------------------------ Register
 
 // register load
-$('#input-tree').on('change', function(e) {
-    var file = e.target.files[0];
+$('#input-tree').on('change', loadInputTree);
+$('#input-tree-reload').click(loadInputTree);
+function loadInputTree() {
+  var file = $('#input-tree')[0].files[0]
+  var reader = new FileReader();
+  reader.onload = function() {
+    tree = JSON.parse(this.result);
+    $('#tree').empty();
+    loadRoot();
+  }
+  console.info('Loading: ' + file.name);
+  reader.readAsText(file);
+}
 
-    var reader = new FileReader();
-    reader.onload = function() {
-      tree = JSON.parse(this.result);
-      loadRoot();
-    }
-    console.info('Loading: ' + file.name);
-    reader.readAsText(file);
-});
+$('#input-tree-meta').on('change', loadInputTreeMeta);
+$('#input-tree-meta-reload').click(loadInputTreeMeta);
+function loadInputTreeMeta() {
+  var file = $('#input-tree-meta')[0].files[0];
+  var reader = new FileReader();
+  reader.onload = function() {
+    meta = JSON.parse(this.result);
+    loadMeta();
+  }
+  console.info('Loading meta: ' + file.name);
+  reader.readAsText(file);
+}
 
 // register goto
 $('#goto-box').submit(function(e) {
@@ -48,17 +64,21 @@ function on_NodeCollapse(id) {
   $('#' + id + '> .controls > a').text('[+]');
 }
 
-
 // ------------------------------------------------------------ Logic
+
+// Expand folds to target node.
+function expandToNode(address) {
+  var id = addr2id(address);
+  for (var i in address) {
+    $('#' + addr2id(address.slice(0, i)) + '> .body').collapse('show');
+  }
+}
 
 // Expand folds to target node, scroll there and highlight target.
 function gotoNode(address) {
   var id = addr2id(address);
 
-  // expand folds
-  for (var i = 0; i < address.length; i++) {
-    $('#' + addr2id(address.slice(0, i)) + '> .body').collapse('show');
-  }
+  expandToNode(address);
 
   // scroll to target
   $('html, body').animate({
@@ -77,19 +97,76 @@ function mkNode(address) {
   var node = resolve(address);
   var id = addr2id(address);
 
+  // node highlighting
+  var hl = '';
+  if (isType(node)) {
+    hl = 'node-hl-type';
+  } else if (node['Value'] || node['Kind'] == 'Literal') {
+    hl = 'node-hl-value';
+  } else if (node['Kind'] == 'Variable') {
+    hl = 'node-hl-variable';
+  }
+
+  // add type
+  var type = '';
+  if (node['Kind'] == 'Types') {
+    type = getTypes(node).map(function(t) {
+        return '<span class="label label-info">' + t + '</span>';
+    }).join(' ');
+  } else if (node['Kind'] == 'FunctionType') {
+    var param = getTypes(tree[node['Children'][0]]).join(', ');
+    var ret = getTypes(tree[node['Children'][1]]);
+    type = '<span class="label label-info">( ' + param + ' ) -> ' + ret + '</span>';
+  } else if (node['Kind'] == 'TypeVariable') {
+    var type = tree[node['Children'][0]]['Value'];
+    type = '<span class="label label-info">' + type + '</span>';
+  } else {
+    type = '<span class="label label-info">' + getTypes(node) + '</span>';
+  }
+
+  // add value
+  var value = '';
+  if (node['Value']) {
+    value = '<span class="label label-default">' + node['Value'] + '</span>';
+  } else if(node['Kind'] == 'Literal') {
+    value = tree[node['Children'][1]]['Value'];
+    value = '<span class="label label-default">' + value + '</span>';
+  }
+
+  // add variable
+  var variable = '';
+  if (node['Kind'] == 'Variable') {
+    variable = tree[node['Children'][1]]['Value'];
+    variable = '<span class="label label-danger">' + variable + '</span>';
+  } else if (node['Kind'] == 'DeclarationStmt') {
+    variable = tree[node['Children'][1]];
+    variable = tree[variable['Children'][1]]['Value'];
+    variable = '<span class="label label-danger">' + variable + '</span>';
+  } else if (node['Kind'] == 'Parameters') {
+    variable = node['Children'].map(function(i) {
+      var v = tree[i];
+      v = tree[v['Children'][1]]['Value'];
+      return '<span class="label label-danger">' + v + '</span>';
+    }).join(' ');
+  }
+
   var body = "";
   if (node['Value']) {
     body = $('<pre>').text('Value: ' + node['Value']);
   }
 
-  return $('<div class="node" >').attr('id', id).append(
+  return $('<div class="node" >').attr('id', id).addClass(hl).append(
     $('<div class="controls">').append(
       $('<a role="button" data-toggle="collapse">').attr('data-target', '#' + id + '-body').text('[+]')
     ),
 
     $('<div class="displaytext">').append(
+      $('<span class="address">').text('[ ' + id + ' ]'),
       $('<span class="kind">').text(node['Kind']),
-      $('<span class="address">').text('[ ' + id + ' ]')
+      $('<span class="type">').html(type),
+      $('<span class="value">').html(value),
+      $('<span class="variable">').html(variable),
+      $('<span class="meta-label">')
     ),
 
     $('<div class="body collapse">').attr('id', id + '-body').append(
@@ -124,6 +201,31 @@ function loadRoot() {
   $('#tree').html(mkNode([]));
 }
 
+// ------------------------------------------------------------ Meta
+
+function loadMeta() {
+  for (var i in meta['expandTo']) {
+    expandToNode(id2addr(meta['expandTo'][i]));
+  }
+
+  gotoNode(id2addr(meta['goto']));
+  $('#goto-box > input').val(meta['goto']);
+
+  for (var id in meta['labels']) {
+    $('#' + id + '> .displaytext > .meta-label').html(
+      '<span class="label label-success">' + meta['labels'][id] + '</span>'
+    );
+  }
+
+  for (var i in meta['highlights']) {
+    $('#' + meta['highlights'][i]).addClass('node-hl');
+  }
+
+  for (var id in meta['body']) {
+    $('#' + id + '> .body > .text').html(meta['body'][id]);
+  }
+}
+
 // ------------------------------------------------------------ Utilites
 
 function id2addr(id) {
@@ -143,23 +245,64 @@ function resolve(address) {
 
   // check cache
   var node = resolveCache[id];
-
-  if (!node) {
-    node = tree['root']
-    for (var i = 0; i < address.length; i++) {
-      node = tree[node]['Children'][address[i]];
-    }
-    node = tree[node];
-    resolveCache[id] = node;
+  if (node) {
+    return node;
   }
+
+  // walk
+  node = tree['root']
+  for (var i = 0; i < address.length; i++) {
+    node = tree[node]['Children'][address[i]];
+  }
+  node = tree[node];
+  resolveCache[id] = node;
 
   return node;
 }
 
-function forAllNodes(f, address = []) {
-  var node = resolve(address);
-  f(address, node);
-  for (var i = 0; i < node['Children'].length; i++) {
-    forAllNodes(f, address.concat([i]));
+function forAllNodes(f, node=undefined, address=[]) {
+  if (!node) {
+    node = tree[tree['root']];
   }
+
+  f(address, node);
+
+  for (var i in node['Children']) {
+    forAllNodes(f, tree[node['Children'][i]], address.concat([i]));
+  }
+}
+
+function isType(node) {
+  return (
+    node['Kind'] == 'Types'
+    || node['Kind'] == 'FunctionType'
+    || node['Kind'] == 'GenericType'
+    || node['Kind'] == 'TupleType'
+    || node['Kind'] == 'TypeVariable'
+  );
+}
+
+function getTypes(node) {
+
+  if (node['Kind'] == 'GenericType') {
+    return tree[node['Children'][0]]['Value'];
+  }
+
+  if (node['Kind'] == 'TupleType') {
+    return '(' + node['Children'].map(function(gentype) {
+        return getTypes(tree[gentype]);
+    }).join(' , ') + ')';
+  }
+
+  if (node['Kind'] == 'Types') {
+    return node['Children'].map(function(type) {
+        return getTypes(tree[type]);
+    });
+  }
+
+  if (node['Kind'] == 'TypeVariable') {
+    return tree[node['Children'][0]]['Value'];
+  }
+
+  return '';
 }
